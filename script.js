230a3ecf331da5d67f1af3ef7682f98d30676c18
Murillo Vinícius <Murillovinicius13@hotmail.com>
2023-01-16 19:45:36 -0300

    Postando o projeto completo
    Nesse commit, estou postando o projeto 100% funcional mostrando os gráficos.

--- script.js
+++ script.js
@@ -1,45 +1,75 @@
+const btnOne = document.querySelector('#buttonOne')
+const title = document.querySelector('#title')
+          
+btnOne.addEventListener('click', e =>{
+    
+    title.textContent = "10 Países mais populosos do mundo"
+      
+    google.charts.load('current', {packages: ['corechart']})
+    google.charts.setOnLoadCallback(drawChart)
 
-let topDez 
-
-fetch("https://restcountries.com/v3.1/all")
-    .then(response => response.json())
-    .then(dados => {
-        let dadosOrdenados = dados.sort((a, b) => b.population - a.population)
+    function drawChart(){
+        const container = document.querySelector("#chart")
+        const data = new google.visualization.arrayToDataTable([
+            ['Country', 'Population', { role: 'style'}],
+            ['World', 8000000000, '#FFA500'],
+            ['China', 1426000000, '#FFA500'],
+            ['Índia', 1412000000, '#FFA500'],
+            ['EUA', 337000000, 'color:#FFA500;'],
+            ['Indónésia', 275000000, '#FFA500'],
+            ['Paquistão', 234000000, '#FFA500'],
+            ['Nigéria', 216000000, '#FFA500'],
+            ['Brasil', 215000000, '#FFA500'],
+            ['Bangladesh', 170000000, '#FFA500'],
+            ['Russia', 127000000,'#FFA500' ]
+        ])
         
-        let topDez = dadosOrdenados.slice(0, 10)
+        const options = {
+            width: 900,
+            height: 500,
+            bar: {groupWidth: "90%"},
+            backgroundColor: '#e9e9e9'
+        }
+        
+        const chart = new google.visualization.BarChart(container)
+        chart.draw(data, options)
+      }
+      
+      })
 
 
+const btnTwo = document.querySelector('#buttonTwo')
 
+btnTwo.addEventListener('click', e =>{
+    title.textContent = '10 Idiomas mais falados do mundo'
 
-    })
+    google.charts.load('current', {packages: ['coreChart']})
+    google.charts.setOnLoadCallback(drawChart)
 
-    google.charts.load('current', {'packages':['corechart']});
-    google.charts.setOnLoadCallback(drawChart);
-    
-    function drawChart() {
-        let data = new google.visualization.DataTable();
-        data.addColumn('string', 'País');
-        data.addColumn('number', 'População');
-    
-        topDez.forEach(pais => {
-            data.addRow([pais.name.common, pais.population]);
-        });
-    
-        let options = {
-            title: 'Países com maior população',
-            chartArea: {width: '50%'},
-            hAxis: {
-                title: 'População',
-                minValue: 0
-            },
-            vAxis: {
-                title: 'País'
-            }
-        };
-    
-        let chart = new google.visualization.BarChart(document.getElementById('chart'));
-        chart.draw(data, options);
+    function drawChart(){
+        const container = document.querySelector('#chart')
+        const data = new google.visualization.arrayToDataTable([
+            ['Idiomas', 'Quantidade', {role: 'style'}],
+            ['Inglês', 1500000000, '#FFA500'],
+            ['Mandarim',1100000000, '#FFA500'],
+            ['Hindi',602200000, '#FFA500'],
+            ['Espanhol',538300000, '#FFA500'],
+            ['Francês',274100000, '#FFA500'],
+            ['Árabe',274000000, '#FFA500'],
+            ['Bengali',272200000, '#FFA500'],
+            ['Russo',258200000, '#FFA500'],
+            ['Português',257700000, '#FFA500'],
+            ['Urdu',231300000, '#FFA500']
+        ])
+
+        const options = {
+            width: 900,
+            height: 500,
+            bar: {groupWidth: "90%"},
+            backgroundColor: '#e9e9e9'
+        }
+
+        const chart = new google.visualization.BarChart(container)
+        chart.draw(data, options)
     }
-    
-
-    
+})
